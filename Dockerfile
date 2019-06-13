cbb54234a96cf99c1b0f0259f61f7ddf6495c38d
dahnbalan <dahnbalan@dal.ca>
2019-06-13 02:25:30 -0300

    docker changes

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,4 @@ RUN npm install
 RUN npm install -g @angular/cli
 COPY . . 
 CMD ng serve --host 0.0.0.0 
+EXPOSE 80
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://cc-a3-backend.azurewebsites.net'
 };
